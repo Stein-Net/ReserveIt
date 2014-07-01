13f331056ce3f54fa7bb269439344a3e61ce0c57
Gregory Rudolph <gregory@stein13.net>
2014-07-01 00:01:11 -0400

    If at first you don't succeed...

--- app/src/main/java/net/stein13/reserveit/newReservation.java
+++ app/src/main/java/net/stein13/reserveit/newReservation.java
@@ -1,26 +1,31 @@
 package net.stein13.reserveit;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.Explode;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.view.Window;
 
+import net.stein13.reserveit.R;
 
-public class WelcomeActivity extends Activity {
-// Hi Hunter!
+public class newReservation extends Activity {
+
+    @TargetApi(Build.VERSION_CODES.L)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_new_reservation);
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.welcome, menu);
+        getMenuInflater().inflate(R.menu.new_reservation, menu);
         return true;
     }
 
@@ -34,11 +39,5 @@ public class WelcomeActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-
-    public void NewReservation(View view) {
-        Intent intent = new Intent(this, newReservation.class);
-        startActivity(intent);
     }
 }
