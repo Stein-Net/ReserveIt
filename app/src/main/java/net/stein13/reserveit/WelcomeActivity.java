0f55d9f58c360e3f47e6db8770cf30a932ed074c
Gregory Rudolph <gregory@stein13.net>
2014-06-30 23:19:23 -0400

    Huuunterrrrr

--- app/src/main/java/net/stein13/reserveit/WelcomeActivity.java
+++ app/src/main/java/net/stein13/reserveit/WelcomeActivity.java
@@ -1,9 +1,11 @@
 package net.stein13.reserveit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class WelcomeActivity extends Activity {
@@ -33,4 +35,6 @@ public class WelcomeActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
