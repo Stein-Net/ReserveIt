178116619d750ca28204db9ceea557f07e19b90e
Gregory Rudolph <gregory@stein13.net>
2014-07-04 08:11:39 -0400

    I think I have the insert function working... I think... Um...

---
+++ app/src/main/java/net/stein13/reserveit/DBAdapter.java
@@ -0,0 +1,106 @@
+package net.stein13.reserveit;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+public class DBAdapter
+{
+    int id = 0;
+    public static final String TAG = "YOUR-TAG-NAME";
+    public static final String KEY_ROWID = "_id";
+    static final String DATABASE_NAME = "Reservations.db";
+    static final String TABLE_RESERVATIONS = "Reservations";
+    static final String NAME_COLUMN = "Name";
+    static final String EMAIL_COLUMN = "E-mail";
+    static final String PHONENUMBER_COLUMN = "Phone Number";
+    static final String ADDRESS_COLUMN = "Street Address";
+    static final String ZIP_COLUMN = "Zip Code";
+    static final String ARRIVAL_COLUMN = "Arrival Date";
+    static final String DEPARTURE_COLUMN = "Departure Date";
+    static final String NOTES_COLUMN = "Notes Area";
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_CREATE =
+            "create table reservations (_id integer primary key autoincrement, "
+                    + "Name text not null );";
+
+    private final Context context;
+
+    private DatabaseHelper DBHelper;
+    private SQLiteDatabase db;
+    public DBAdapter(Context ctx)
+    {
+        this.context = ctx;
+        DBHelper = new DatabaseHelper(context);
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper
+    {
+        DatabaseHelper(Context context)
+        {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db){
+            String makeTable = "CREATE TABLE " + TABLE_RESERVATIONS + "("
+                    + NAME_COLUMN + " TEXT," + EMAIL_COLUMN + " TEXT,"
+                    + PHONENUMBER_COLUMN + " INTEGER," + ADDRESS_COLUMN + " TEXT,"
+                    + ZIP_COLUMN + " TEXT," + ARRIVAL_COLUMN + " INTEGER,"
+                    + DEPARTURE_COLUMN + " INTEGER," + NOTES_COLUMN + " TEXT,"+ ")";
+            db.execSQL(makeTable);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion,
+                              int newVersion)
+        {
+            Log.w(TAG, "Upgrading database from version " + oldVersion
+                    + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS reservations");
+            onCreate(db);
+        }
+    }
+    //---opens the database---
+    public DBAdapter open() throws SQLException
+    {
+        db = DBHelper.getWritableDatabase();
+        return this;
+    }
+
+    //---closes the database---
+    public void close()
+    {
+        DBHelper.close();
+    }
+    public long insertReservation(String nameValue, String  emailValue, String  phoneValue, String  addressValue, String zipValue, String arrivalValue, String departureValue, String notesValue)
+    {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(NAME_COLUMN, nameValue);
+        initialValues.put(EMAIL_COLUMN, emailValue);
+        initialValues.put(PHONENUMBER_COLUMN, phoneValue);
+        initialValues.put(ADDRESS_COLUMN, addressValue);
+        initialValues.put(ZIP_COLUMN, zipValue);
+        initialValues.put(ARRIVAL_COLUMN, arrivalValue);
+        initialValues.put(DEPARTURE_COLUMN, departureValue);
+        initialValues.put(NOTES_COLUMN, notesValue);
+
+        return db.insert(TABLE_RESERVATIONS, null, initialValues);
+    }
+    public int getAllEntries()
+    {
+        Cursor cursor = db.rawQuery(
+                "SELECT COUNT(Quote) FROM tblReservations", null);
+        if(cursor.moveToFirst()) {
+            return cursor.getInt(0);
+        }
+        return cursor.getInt(0);
+
+    }
+}
--- app/src/main/java/net/stein13/reserveit/newReservation.java
+++ app/src/main/java/net/stein13/reserveit/newReservation.java
@@ -8,12 +8,15 @@ import android.os.Bundle;
 import android.transition.Explode;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import net.stein13.reserveit.R;
 
 public class newReservation extends Activity {
-
+    DBAdapter db = new DBAdapter(this);
     @TargetApi(Build.VERSION_CODES.L)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,5 +48,30 @@ public class newReservation extends Activity {
     public void tellAbout(MenuItem item) {
         Intent intent = new Intent(this, aboutApp.class);
         startActivity(intent);
+    }
+
+    public void createReservation(View view) {
+        db.open();
+        EditText nameText = (EditText)findViewById(R.id.nameText);
+        String nameValue = nameText.getText().toString();
+        EditText emailText = (EditText)findViewById(R.id.emailText);
+        String emailValue = emailText.getText().toString();
+        EditText phoneNumber = (EditText)findViewById(R.id.phoneNumber);
+        String phoneValue = phoneNumber.getText().toString();
+        EditText addressText = (EditText)findViewById(R.id.addressText);
+        String addressValue = addressText.getText().toString();
+        EditText zipNumber = (EditText)findViewById(R.id.zipNumber);
+        String zipValue = zipNumber.getText().toString();
+        EditText arrivalDate = (EditText)findViewById(R.id.arrivalDate);
+        String arrivalValue = arrivalDate.getText().toString();
+        EditText departureDate = (EditText)findViewById(R.id.departureDate);
+        String departureValue = departureDate.getText().toString();
+        EditText notesArea = (EditText)findViewById(R.id.notesText);
+        String notesValue = notesArea.getText().toString();
+
+        db.insertReservation(nameValue, emailValue, phoneValue, addressValue, zipValue, arrivalValue, departureValue, notesValue);
+        Toast toast = Toast.makeText(getApplicationContext(), "Creating Reservation", Toast.LENGTH_LONG);
+        toast.show();
+        db.close();
     }
 }
